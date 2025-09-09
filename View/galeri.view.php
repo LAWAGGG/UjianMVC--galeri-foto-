9590b40ad1c9be3dba623a80269730420451fcc8
LAWAGGG <ahmadfagih.arrifai@gmail.com>
2025-09-09 08:00:57 +0700

    adding styling again

--- View/galeri.view.php
+++ View/galeri.view.php
@@ -1,37 +1,43 @@
-<?php 
-    class GaleriView extends GaleriController{
-        public function show(){
-            $galeries = $this->getAll();
+<?php
+class GaleriView extends GaleriController
+{
+    public function show()
+    {
+        $galeries = $this->getAll();
 
-            foreach($galeries as $galeri){
-                ?>
-                    <tr>
-                        <td><?php echo $galeri['id'] ?></td>
-                        <td class="image-cell"><img src="<?php echo $galeri['nama_foto'] ?>" alt=""></td>
-                        <td><?php echo $galeri['informasi_foto'] ?></td>
-                        <td><?php echo $galeri['tanggal_foto'] ?></td>
-                    </tr>
-                <?php
-            }
-        }
-
-         public function find(){
-            $galeries = $this->getOne();
-
-            if(empty($galeries)){
-                return;
-            }
-
-            foreach($galeries as $galeri){
-                ?>
-                    
-                        <p>No : <?php echo $galeri['id'] ?></p>
-                        <td class="image-cell"><img src="<?php echo $galeri['nama_foto'] ?>" alt=""></td>
-                        <h3> Informasi Foto : <?php echo $galeri['informasi_foto'] ?></h3>
-                        <p>Tanggal Foto : <?php echo $galeri['tanggal_foto'] ?></p>
-                    
-                <?php
-            }
+        foreach ($galeries as $galeri) {
+?>
+            <tr>
+                <td><?php echo $galeri['id'] ?></td>
+                <td class="image-cell"><img src="<?php echo $galeri['nama_foto'] ?>" alt=""></td>
+                <td><?php echo $galeri['informasi_foto'] ?></td>
+                <td><?php echo $galeri['tanggal_foto'] ?></td>
+            </tr>
+        <?php
         }
     }
+
+    public function find()
+    {
+        $galeries = $this->getOne();
+
+        if (empty($galeries)) {
+            return;
+        }
+
+        foreach ($galeries as $galeri) {
+        ?>
+            <div class="imageFind">
+                <td class="image-cell"><img src="<?php echo $galeri['nama_foto'] ?>" alt=""></td>
+                <div class="details">
+                    <p>No : <?php echo $galeri['id'] ?></p>
+                    <h3> Informasi Foto : <?php echo $galeri['informasi_foto'] ?></h3>
+                    <p>Tanggal Foto : <?php echo $galeri['tanggal_foto'] ?></p>
+                </div>
+            </div>
+
+<?php
+        }
+    }
+}
 ?>
